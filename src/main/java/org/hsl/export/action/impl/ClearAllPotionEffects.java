7f53aa04d6f2bc49062868aa6f7705d0a5fd9c11
AdvancedAntiSkid <abel.herfort@gmail.com>
2025-08-11 15:52:36 +0200

    fix(export): fix action constructors

--- src/main/java/org/hsl/export/action/impl/ClearAllPotionEffects.java
+++ src/main/java/org/hsl/export/action/impl/ClearAllPotionEffects.java
@@ -2,13 +2,11 @@ package org.hsl.export.action.impl;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import org.hsl.export.action.Action;
 import org.hsl.export.action.ActionType;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Accessors(fluent = true)
 @Getter
 public class ClearAllPotionEffects implements Action {
