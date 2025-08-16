0d56cedaf430552811a75727a4d217ae4606bac6
AdvancedAntiSkid <abel.herfort@gmail.com>
2025-08-17 01:05:41 +0200

    feat(importer): add module bridge

--- src/main/java/org/housingstudio/hsl/importer/ModuleBridge.java
+++ src/main/java/org/housingstudio/hsl/importer/ModuleBridge.java
@@ -1,4 +1,45 @@
 package org.housingstudio.hsl.importer;
 
-public class ModuleBridge {
+import org.housingstudio.hsl.exporter.Metadata;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+/**
+ * Represents a bridge between the HSL implementation and a ChatTriggers module.
+ */
+public interface ModuleBridge {
+    /**
+     * Handle module post load event.
+     */
+    void onLoad();
+
+    /**
+     * Handle module post unload event.
+     */
+    void onUnload();
+
+    /**
+     * Import a game into a Hypixel House.
+     *
+     * @param id the unique name of the name
+     * @return {@code true} if the game was imported, {@code false} otherwise
+     */
+    boolean importGame(@NotNull String id);
+
+    /**
+     * Retrieve the list of games placed in the `games` directory.
+     *
+     * @return the identifiers of the recognized games
+     */
+    @NotNull List<String> listGames();
+
+    /**
+     * Retrieve the metadata of the specified game.
+     *
+     * @param id the unique name of the name
+     * @return the metadata of the game, or {@code null} if the game is not recognized
+     */
+    @Nullable Metadata loadMetadata(@NotNull String id);
 }
---
+++ src/main/java/org/housingstudio/hsl/importer/ModuleBridgeProvider.java
@@ -0,0 +1,10 @@
+package org.housingstudio.hsl.importer;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ModuleBridgeProvider {
+    private static ModuleBridge bridge;
+}
