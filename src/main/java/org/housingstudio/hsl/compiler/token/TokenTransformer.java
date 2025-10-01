2d81c6514184dc5a946f6718dea4511e5c853bbc
AdvancedAntiSkid <abel.herfort@gmail.com>
2025-10-01 17:00:50 +0200

    fix(compiler): forbid auto semicolon before rparen token

--- src/main/java/org/housingstudio/hsl/compiler/token/TokenTransformer.java
+++ src/main/java/org/housingstudio/hsl/compiler/token/TokenTransformer.java
@@ -66,7 +66,8 @@ public class TokenTransformer {
         Token.of(TokenType.OPERATOR, "$"),
         Token.of(TokenType.OPERATOR, "."),
         Token.of(TokenType.OPERATOR, "%"),
-        Token.of(TokenType.OPERATOR, "|")
+        Token.of(TokenType.OPERATOR, "|"),
+        Token.of(TokenType.RPAREN,   ")")
     );
 
     /**
